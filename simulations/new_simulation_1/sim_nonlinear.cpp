13facd55135d5da07ab7d43ab1a8ada1a11037dd
aldoclemente <aldo.clemente@polimi.it>
2025-10-23 20:43:40 +0200

    minor changes

--- simulations/new_simulation_1/sim_nonlinear.cpp
+++ simulations/new_simulation_1/sim_nonlinear.cpp
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]){
     std::string data_dir = "data";
     std::string mesh_dir = "data/mesh/";
     
-    int n_locs = std::stoi(argv[1]); // 0 -> "100", 1 -> "250", 3 -> "500", 4 "1000" 
+    std::string n_locs = std::string(argv[1]); // "100", "250" "500" "1000" 
     std::string sim = std::string(argv[2]); // 0, ..., 29
 
     auto nodes = read_mtx<double>(mesh_dir + "points.mtx");
@@ -34,48 +34,50 @@ int main(int argc, char *argv[]){
     vector_t time_mesh = read_mtx<double>(mesh_dir + "time_mesh_pde_sol.mtx");
     int n_times = time_mesh.size();
     
-    std::vector<std::string> sigma_lab = {"100", "250", "500", "1000"};
-    std::string sigma_dir = data_dir + "/" + sigma_lab[sd] + "/";
     vector_t rmse = vector_t::Zero(1);
-    std::string sim_dir = sigma_dir + sim + "/";
+    std::string sim_dir = data_dir + "/" + n_locs + "/" + sim + "/";
 
     matrix_t locs = read_mtx<double>(sim_dir + "locs.mtx");
     matrix_t obs = read_mtx<double>(sim_dir + "obs.mtx");
 
     // IC estimate -------------------------------------------------------------------------------
-    int n_lambda = 50
-    auto lambda_grid = vector_t::Ones(n_lambda);
+    TrialFunction u(Vh);
+    TestFunction v(Vh);
+    ZeroField<2> f;
+
+    int n_lambda = 50;
+    vector_t lambda_grid = vector_t::Ones(n_lambda);
     for(int i=0; i<n_lambda;++i) lambda_grid(i,0) = std::pow(10, -4. + 0.15 * i);
 
     GeoFrame data_IC(unit_square);
-    auto &l_IC = data.insert_scalar_layer<POINT>(
+    auto &l_IC = data_IC.insert_scalar_layer<POINT>(
                 "layer", locs); 
-    l.load_vec("y", obs.col(0).reshaped()); 
+    l_IC.load_vec("y", obs.col(0).reshaped()); 
 
     double mu =  1.;// 0.01;
-    double alpha = 10.0; //3.0;
+    double r = 10.0; //3.0;
 
     auto a = integral(unit_square)(mu*dot(grad(u), grad(v)));
     auto F = integral(unit_square)(f * v);
-    SRPDE model_IC("y ~ f", data, fe_ls_elliptic(a, F));
+    SRPDE model_IC("y ~ f", data_IC, fe_ls_elliptic(a, F));
 
     GridSearch<1> optimizer_IC;
     optimizer_IC.optimize(model_IC.gcv(100, 476813), lambda_grid);
-    model_IC.fit(optimizer.optimum());
+    model_IC.fit(optimizer_IC.optimum());
     vector_t IC = model_IC.f();
     // -------------------------------------------------------------------------------------------
     
     matrix_t time_locs = time_mesh.block(1, 0, time_mesh.size()-1, 1);
-    int n_times = time_mesh.rows();
-    int n_sim = 30;
+    matrix_t obs_nonlin = obs.block(0,1, locs.rows(), time_mesh.size()-1);
+    // obs.rightCols(time_mesh.size()-1)
     Triangulation<1, 1> T(time_mesh);
-
+    std::cout << time_mesh.transpose() << std::endl;
     GeoFrame data(unit_square, T);
     auto &l = data.insert_scalar_layer<POINT, POINT>(
                 "layer", std::pair{locs, time_locs});
-    l.load_vec("y", obs.rightCols(n_times-1).reshaped() ); 
-            
-    SRPDE nonlinear("y~f", data, fe_ls_fisher_kpp(std::pair(mu,alpha), Vh, IC)); 
+    l.load_vec("y", obs_nonlin.reshaped() ); 
+    std::cout << "NONLINEAR" << std::endl;
+    SRPDE nonlinear("y~f", data, fe_ls_fisher_kpp(std::pair(mu,r), Vh, IC)); 
     LBFGS<Dynamic> lbfgs;
     int n_dofs = nodes.rows();
     vector_t g_init = 0.5*(vector_t::Random(n_dofs*time_locs.size()) + vector_t::Ones(n_dofs*time_locs.size()));
@@ -83,12 +85,12 @@ int main(int argc, char *argv[]){
     
     // 10-folds CV to select lambda -----------------------------------------------------------------------------------
     int K = 10;
-    KFoldCV cv_(locs, obs, K);
+    KFoldCV cv_(locs, obs_nonlin, K);
     vector_t cv_error = vector_t::Ones(K);
 
     ScalarField<1> SSE;
 
-    SSE = [&unit_square, &T, &cv_, &time_locs, &IC, &Vh, &mu, &alpha, &n_times, &n_dofs, &g_init](vector_t lambda) -> double {
+    SSE = [&unit_square, &T, &cv_, &time_locs, &IC, &Vh, &mu, &r, &n_times, &n_dofs, &g_init](vector_t lambda) -> double {
         if (lambda[0] <= 0.0) return std::numeric_limits<double>::max();
         double res = 0;
         LBFGS<Dynamic> lbfgs;
@@ -99,9 +101,9 @@ int main(int argc, char *argv[]){
 
             GeoFrame data(unit_square, T);
             auto &l = data.insert_scalar_layer<POINT, POINT>("layer", std::pair{locs, time_locs});
-            l.load_vec("y", obs.rightCols(n_times-1).reshaped() ); 
+            l.load_vec("y", obs.reshaped() ); 
             
-            SRPDE model("y~f", data, fe_ls_fisher_kpp(std::pair(mu,alpha), Vh, IC)); 
+            SRPDE model("y~f", data, fe_ls_fisher_kpp(std::pair(mu,r), Vh, IC)); 
             
             //vector_t g_init = 0.5*(vector_t::Random(n_dofs*time_locs.size()) + vector_t::Ones(n_dofs*time_locs.size()));
             model.fit(lambda[0], g_init, lbfgs); // BacktrackingLineSearch()
@@ -122,8 +124,8 @@ int main(int argc, char *argv[]){
 
     };
 
-    int n_lambda = 11;
-    vector_t lambda_grid = vector_t::Ones(n_lambda);
+    n_lambda = 11;
+    lambda_grid = vector_t::Ones(n_lambda);
     for(int i=0; i<n_lambda;++i) lambda_grid(i,0) = std::pow(10, -1. + 0.15 * i);
     
     GridSearch<1> optimizer;
@@ -144,24 +146,27 @@ int main(int argc, char *argv[]){
     vector_t result = vector_t::Zero(n_times * n_dofs);
     result.topRows(n_dofs) = IC;
     result.bottomRows((n_times-1)*(n_dofs)) = nonlinear.f();
+
+    std::cout << result.rows() << " " << result.cols() << std::endl;
     Eigen::saveMarket(result, sim_dir + "estimate_nonlinear.mtx");
     // ----------------------------------------------------------------------------------------------------------------
 
     // rmse
+    matrix_t test_locs = read_mtx<double>(mesh_dir + "test_locs.mtx");
     auto psi_test = internals::point_basis_eval(Vh, test_locs);
 
-    matrix_t test_vals = matrix_t::Zero(test_locs.rows(), n_times - 1); // t0 butto via
+    matrix_t test_vals = matrix_t::Zero(test_locs.rows(), n_times);
     for(int t = 0; t<n_times; ++t){ 
         test_vals.col(t) = psi_test * result.block(n_dofs*t, 0, n_dofs, 1);    
     }
 
-    matrix_t test_obs = read_mtx<double>(sim_dir + "test_obs.mtx");
+    matrix_t test_obs = read_mtx<double>(mesh_dir + "test_obs.mtx");
     
     rmse[0] = std::sqrt( (test_vals - test_obs).reshaped().array().square().mean()); 
     //std::cout << "rmse nonlinear " << rmse << std::endl;
     Eigen::saveMarket(rmse, sim_dir + "rmse_nonlinear.mtx");
     std::cout << "rmse: " << rmse << std::endl;
-    std::string command_str = "chown -R 1000:1000 " + data_dir; 
+    command_str = "chown -R 1000:1000 " + data_dir; 
     system(command_str.c_str());
     return 0;
 }
