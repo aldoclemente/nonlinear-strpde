5eac20092735ca5e086c74891684c4530a740c21
aldoclemente <aldo.clemente@polimi.it>
2025-11-07 12:13:33 +0100

    no changes.

--- simulations/simulation_2/run_script_cpp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-testing=$HOME/nonlinear-strpde
-cpp=$HOME/fdaPDE-cpp
-core=$cpp/fdaPDE/core
-
-if [ -f script ]; then rm script; fi
-g++ -o script $1 -I$cpp -I$core -I/usr/include/eigen3 -O2 -std=c++20 -march=native -s
-./script
